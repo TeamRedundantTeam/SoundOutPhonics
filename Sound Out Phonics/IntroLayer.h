68cbcdb69a901d0dadb63530ba026f4163136cc0
OlegMatvejev <omatveje@gmail.com>
2013-10-23 20:21:57 -0700

    Add initial Cocos2d-Iphone project

---
+++ Sound Out Phonics/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Sound Out Phonics
+//
+//  Created by Oleg M on 2013-10-23.
+//  Copyright Team Redundant Team 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
