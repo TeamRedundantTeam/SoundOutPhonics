ccd0a3e7ab0aef57981910a3052457178e63cb54
OlegMatvejev <omatveje@gmail.com>
2013-10-27 15:49:38 -0700

    Fix history links in bff9f75a0b4ea015b48740182a89df9f6862b1de

--- Sound Out Phonics/Game/GameLayer.m
+++ Sound Out Phonics/Game/GameLayer.m
@@ -3,7 +3,9 @@
 //  Sound Out Phonics
 //
 //  Purpose: This is the main game class scene and layer that handles all the game logic, displays sprites and performs touch events
-//  History: History of the file can be found here: https://github.com/TeamRedundantTeam/SoundOutPhonics/commits/master/Sound%20Out%20Phonics/Game/GameLayer.m
+//  History: History of the file can be found here:
+//           Version 0.1 - 0.5: https://github.com/TeamRedundantTeam/SoundOutPhonics/commits/master/Sound%20Out%20Phonics/Game/GameBoardLayer.m
+//           Version 0.6 + : https://github.com/TeamRedundantTeam/SoundOutPhonics/commits/master/Sound%20Out%20Phonics/Game/GameLayer.m
 //
 //  Created by Oleg Matvejev on 2013-10-23.
 //  Copyright (c) 2013 Team Redundant Team. All rights reserved.
--- Sound Out Phonics/Game/Slot.h
+++ Sound Out Phonics/Game/Slot.h
@@ -3,7 +3,9 @@
 //  Sound Out Phonics
 //
 //  Purpose: Helper class that handles the slots creation and keeping track of the graphemes that are associated to each of the slot
-//  History: History of the file can be found here: https://github.com/TeamRedundantTeam/SoundOutPhonics/commits/master/Sound%20Out%20Phonics/Game/Slot.h
+//  History: History of the file can be found here:
+//           Version 0.1 - 0.5: https://github.com/TeamRedundantTeam/SoundOutPhonics/commits/master/Sound%20Out%20Phonics/Game/Slots.h
+//           Version 0.6 + : https://github.com/TeamRedundantTeam/SoundOutPhonics/commits/master/Sound%20Out%20Phonics/Game/Slot.h
 //
 //  Created by Oleg Matvejev on 2013-10-26.
 //  Copyright (c) 2013 Team Redundant Team. All rights reserved.
