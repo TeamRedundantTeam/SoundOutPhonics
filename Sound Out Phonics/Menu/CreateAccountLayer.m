fbf095eff049ed5443479b23bd4f4ab3e18e4b9d
eriks86 <Daide@Eriks-iMac.local>
2013-12-01 22:57:41 -0800

    UI pass on edit/create profile
    -Add white cancel icon
    -Add camera icon
    -Change layout

--- Sound Out Phonics/Menu/CreateAccountLayer.m
+++ Sound Out Phonics/Menu/CreateAccountLayer.m
@@ -127,7 +127,7 @@
 
         // Add exit only if we are creating the user account
         if (accountLevel == 0) {
-            _exitButton = [CCSprite spriteWithFile:@"Cancel-IconFinal.png"];
+            _exitButton = [CCSprite spriteWithFile:@"Cancel-IconFinal-White.png"];
             _exitButton.position = ccp(_size.width/2 + 375, _size.height/2 + 290);
             [self addChild:_exitButton];
         }
